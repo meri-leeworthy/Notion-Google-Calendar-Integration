20973a0ccc288a3e3c243489eeeea4e15f60d552
meri-leeworthy <meri.leeworthy@gmail.com>
2021-07-03 14:46:52 +1000

    Create Notion events from Google with start time

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-const getNotionEvents = require("./lib/notion");
+const notion = require("./lib/notion");
 const getCalendarEvents = require("./lib/google");
 
 (async () => {
-  const notionEvents = await getNotionEvents();
+  //   const notionEvents = await notion.getEvents();
   //   console.log(notionEvents);
 
   const nowDate = new Date();
   const oldDate = new Date();
-  oldDate.setMonth(oldDate.getMonth() - 1);
+  oldDate.setDate(oldDate.getDate() - 10);
 
   const calendarEvents = await getCalendarEvents(oldDate, nowDate);
-  console.log(calendarEvents);
+  //   console.log(calendarEvents);
+
+  calendarEvents.forEach((event) => {
+    const notionEvent = {
+      title: event.summary,
+      start: event.start,
+      description: event.description ? event.description : ""
+    };
+
+    event.end ? (notionEvent.end = event.end) : null;
+
+    console.log("google event: ");
+    console.log(event);
+    console.log("notionEvent: ");
+    console.log(notionEvent);
+
+    notion.createEvent(notionEvent);
+  });
+
+  //   notion.createEvent({
+  //     title: "Meri Event 4",
+  //     description: "This is the description of the event",
+  //     date: "2021-07-14"
+  //   });
 })();
+
+//notion.getDatabase() can be used to get the database id or property ids
--- readme.md
+++ readme.md
@@ -5,3 +5,9 @@ https://github.com/RajKKapadia/Google_Calendar_Youtube_Demo/blob/main/google_cal
 https://github.com/CamSkiTheDev/Google-Calendar-NodeJS-App
 
 https://www.youtube.com/watch?v=zrLf4KMs71E&list=TLPQMDIwNzIwMjH3lZoQj5KBVA&index=1
+
+https://developers.google.com/calendar/api/v3/reference/events/list
+
+https://googleapis.dev/nodejs/googleapis/latest/calendar/index.html
+
+https://developers.notion.com/reference/get-databases
