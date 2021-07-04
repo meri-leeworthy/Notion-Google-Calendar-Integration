25176c31b2f33e9ad369c9ce994de0c632f53d42
meri-leeworthy <meri.leeworthy@gmail.com>
2021-07-04 12:50:47 +1000

    update events works

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 const notion = require("./lib/notion");
 const getCalendarEvents = require("./lib/google");
 
-(async () => {
-  //   const notionEvents = await notion.getEvents();
-  //   console.log(notionEvents);
+//format Google Calendar event into the format Notion expects
+//and only send event end time if there is one on the GCal one
+const formatNotionEvent = (event) => {
+  const formattedEvent = {
+    title: event.summary,
+    id: event.id,
+    start: event.start,
+    description: event.description ? event.description : ""
+  };
 
+  event.end ? (formattedEvent.end = event.end) : null;
+
+  return formattedEvent;
+};
+
+const syncNotionToGoogle = (googleEvents, dictionary) => {
+  //iterate through events; for each one, update or create a Notion event
+  googleEvents.forEach((event) => {
+    dictionary[event.id]
+      ? notion.updateEvent(formatNotionEvent(event), dictionary[event.id])
+      : notion.createEvent(formatNotionEvent(event));
+  });
+};
+
+(async () => {
+  //get a dictionary that maps Google calendar IDs to Notion calendar IDs
+  const dictionary = await notion.getEventDictionary();
+
+  //set the range of dates to fetch from Google calendar
   const nowDate = new Date();
   const oldDate = new Date();
   oldDate.setDate(oldDate.getDate() - 10);
 
-  const calendarEvents = await getCalendarEvents(oldDate, nowDate);
-  //   console.log(calendarEvents);
+  //fetch events from Google calendar
+  const googleEvents = await getCalendarEvents(oldDate, nowDate);
 
-  calendarEvents.forEach((event) => {
-    const notionEvent = {
-      title: event.summary,
-      start: event.start,
-      description: event.description ? event.description : ""
-    };
-
-    event.end ? (notionEvent.end = event.end) : null;
-
-    console.log("google event: ");
-    console.log(event);
-    console.log("notionEvent: ");
-    console.log(notionEvent);
-
-    notion.createEvent(notionEvent);
-  });
-
-  //   notion.createEvent({
-  //     title: "Meri Event 4",
-  //     description: "This is the description of the event",
-  //     date: "2021-07-14"
-  //   });
+  syncNotionToGoogle(googleEvents, dictionary);
 })();
 
 //notion.getDatabase() can be used to get the database id or property ids
