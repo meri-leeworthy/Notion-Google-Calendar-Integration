d7ae0f05014af8da9b1ea5fde391bfec7c0e2954
meri-leeworthy <meri.leeworthy@gmail.com>
2021-07-10 11:33:22 +1000

    req.get

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 // Require express and body-parser
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 
 // Initialize express and define a port
 const app = express();
 const PORT = 3000;
 
 // Tell express to use body-parser's JSON parsing
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.post("/hook", (req, res) => {
-  console.log(JSON.stringify(req.body)); // Call your action on the request here
+  console.log("X-Goog-Channel-ID:");
+  console.log(JSON.stringify(req.get("X-Goog-Channel-ID")));
+  console.log("X-Goog-Resource-ID:");
+  console.log(JSON.stringify(req.get("X-Goog-Resource-ID")));
+  console.log("X-Goog-Resource-State:");
+  console.log(JSON.stringify(req.get("X-Goog-Resource-State")));
   res.status(200).end(); // Responding is important
 });
 
--- readme.md
+++ readme.md
@@ -11,3 +11,5 @@ https://developers.google.com/calendar/api/v3/reference/events/list
 https://googleapis.dev/nodejs/googleapis/latest/calendar/index.html
 
 https://developers.notion.com/reference/get-databases
+
+https://www.reddit.com/r/ruby/comments/gv5hdk/integrating_with_google_calendar_the_right_way/
