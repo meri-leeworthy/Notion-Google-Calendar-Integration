b56de013fee273bb5d605b0f947943d192eab016
meri-leeworthy <meri.leeworthy@gmail.com>
2021-07-13 23:03:33 +1000

    json

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ app.get("/", (req, res) => {
 
 app.post("/hook", (req, res) => {
   console.log(JSON.stringify(req.body));
+  console.log(req.baseUrl);
   console.log("X-Goog-Channel-ID:");
-  console.log(req.get("X-Goog-Channel-ID"));
+  console.log(JSON.stringify(req.get('X-Goog-Channel-ID')));
   console.log("X-Goog-Resource-ID:");
-  console.log(req.get("X-Goog-Resource-ID"));
+  console.log(req.get('X-Goog-Resource-ID'));
   console.log("X-Goog-Resource-State:");
-  console.log(req.get("X-Goog-Resource-State"));
+  console.log(req.get('X-Goog-Resource-State'));
   res.status(200).end(); // Responding is important
 });
 
