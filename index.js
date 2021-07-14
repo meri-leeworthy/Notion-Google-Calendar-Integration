8f5ca450e508ccdb64fcdd088624d0e1dba9c7b8
meri-leeworthy <meri.leeworthy@gmail.com>
2021-07-14 12:32:54 +1000

    google site verification

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Require express and body-parser
 const express = require("express");
+const path = require("path");
 // const bodyParser = require("body-parser");
 
 // Initialize express and define a port
@@ -13,15 +14,19 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/google96814f8a402614e3.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "/google96814f8a402614e3.html"));
+});
+
 app.post("/hook", (req, res) => {
   console.log(JSON.stringify(req.body));
   console.log(req.baseUrl);
   console.log("X-Goog-Channel-ID:");
-  console.log(JSON.stringify(req.get('X-Goog-Channel-ID')));
+  console.log(JSON.stringify(req.get("X-Goog-Channel-ID")));
   console.log("X-Goog-Resource-ID:");
-  console.log(req.get('X-Goog-Resource-ID'));
+  console.log(req.get("X-Goog-Resource-ID"));
   console.log("X-Goog-Resource-State:");
-  console.log(req.get('X-Goog-Resource-State'));
+  console.log(req.get("X-Goog-Resource-State"));
   res.status(200).end(); // Responding is important
 });
 
